b6bd741853a9d504c4dfe036fe86a98dd55fb7ab
MikyPopescu <mihaellap42@gmail.com>
2021-01-04 12:46:12 +0200

    paypal

--- backend/server.js
+++ backend/server.js
@@ -21,8 +21,10 @@ app.get("/",(req,res) => {
 })
 
 app.use("/api/products", productRoutes)
-app.use("/api/users" ,userRoutes)
-app.use("/api/orders" ,orderRoutes)
+app.use("/api/users", userRoutes)
+app.use("/api/orders", orderRoutes)
+
+app.get('api/config/paypal',(req,res) => res.send(process.env.PAYPAL_CLIENT_ID))
 
 //middleware
 app.use(notFound)
