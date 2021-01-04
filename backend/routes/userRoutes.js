d62adc71b31674037e9ced26220ecb384ac29374
MikyPopescu <mihaellap42@gmail.com>
2021-01-04 03:29:59 +0200

    user functionality

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,7 +3,8 @@ const router = express.Router()
 import {
   authUser,
   getUserProfile,
-  registerUser
+  registerUser,
+  updateUserProfile
 } from '../controllers/userController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
@@ -12,6 +13,7 @@ router.post('/login', authUser)
 router
   .route('/profile')
   .get(protect, getUserProfile)
+  .put(protect, updateUserProfile)
 
 
 export default router
