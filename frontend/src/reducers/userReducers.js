7a33eda20356962c7f9614239ca194baf82aa34f
MikyPopescu <mihaellap42@gmail.com>
2021-01-05 10:53:46 +0200

    frontend edit user

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -3,9 +3,9 @@ import {
   USER_DETAILS_REQUEST,
   USER_DETAILS_RESET,
   USER_DETAILS_SUCCESS,
-  USER_LIST_FAIL,
   USER_LIST_REQUEST,
   USER_LIST_SUCCESS,
+  USER_LIST_FAIL,
   USER_LIST_RESET,
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
@@ -20,6 +20,10 @@ import {
   USER_DELETE_REQUEST,
   USER_DELETE_SUCCESS,
   USER_DELETE_FAIL,
+  USER_UPDATE_RESET,
+  USER_UPDATE_REQUEST,
+  USER_UPDATE_SUCCESS,
+  USER_UPDATE_FAIL,
 } from '../constants/userConstants'
 
 export const userLoginReducer = (state = {}, action) => {
@@ -78,7 +82,7 @@ export const userUpdateProfileReducer = (state = {}, action) => {
   }
 }
 
-export const userListReducer = (state = {users:[]}, action) => {
+export const userListReducer = (state = { users: [] }, action) => {
   switch (action.type) {
     case USER_LIST_REQUEST:
       return { loading: true }
@@ -87,14 +91,13 @@ export const userListReducer = (state = {users:[]}, action) => {
     case USER_LIST_FAIL:
       return { loading: false, error: action.payload }
     case USER_LIST_RESET:
-      return {users: []}
+      return { users: [] }
     default:
       return state
   }
 }
 
-
-export const userDeleteReducer = (state = { }, action) => {
+export const userDeleteReducer = (state = {}, action) => {
   switch (action.type) {
     case USER_DELETE_REQUEST:
       return { loading: true }
@@ -102,6 +105,23 @@ export const userDeleteReducer = (state = { }, action) => {
       return { loading: false, success: true }
     case USER_DELETE_FAIL:
       return { loading: false, error: action.payload }
+    default:
+      return state
+  }
+}
+
+export const userUpdateReducer = (state = { user: {} }, action) => {
+  switch (action.type) {
+    case USER_UPDATE_REQUEST:
+      return { loading: true }
+    case USER_UPDATE_SUCCESS:
+      return { loading: false, success: true }
+    case USER_UPDATE_FAIL:
+      return { loading: false, error: action.payload }
+    case USER_UPDATE_RESET:
+      return {
+        user: {},
+      }
     default:
       return state
   }
